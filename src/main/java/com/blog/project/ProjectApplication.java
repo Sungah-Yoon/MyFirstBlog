c636909da46620d1d1490ad2f0c932522c259c96
Sungah <sungah0815@gmail.com>
2023-09-01 02:47:21 +0900

    git commit -m "refactor: ProjectApplication 수정"
    간단한 어플리케이션 test 위한 코드이므로 해당 파일에서 삭제,
    controller test 파일을 새롭게 만들어 코드 위치시킴

--- src/main/java/com/blog/project/ProjectApplication.java
+++ src/main/java/com/blog/project/ProjectApplication.java
@@ -3,22 +3,11 @@ package com.blog.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@RestController
 public class ProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
-    }
-
-    @RequestMapping("/")
-    public String hello(){
-        System.out.println("hello world");
-        return "<h1>hello world!!!</h1>";
     }
 }
 

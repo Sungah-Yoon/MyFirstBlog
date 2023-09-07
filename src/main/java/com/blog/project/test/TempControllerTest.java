aeca2d8336f16fd06b839ad503bc0a3581cb935f
Sungah <sungah0815@gmail.com>
2023-09-07 17:02:00 +0900

    git commit -m "test: 페이지 연결 테스트"

---
+++ src/main/java/com/blog/project/test/TempControllerTest.java
@@ -0,0 +1,18 @@
+package com.blog.project.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TempControllerTest {
+
+    // http://localhost:8085/blog/temp/home
+    @GetMapping("/temp/home")
+    public String tempHome(){
+        System.out.println("tempHome()");
+        // 파일리턴 기본경로: src/main/resources/static
+        // 리턴명: /home.html
+        // 풀경로: src/main/resources/static/home.html
+        return "/home.html";
+    }
+}

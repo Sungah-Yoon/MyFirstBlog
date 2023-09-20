c9efd4261f43ced281e09f917e831355d399be73
Sungah <sungah0815@gmail.com>
2023-09-20 16:58:16 +0900

    git commit -m "feat(controller): userService 이용해 회원가입 진행"

--- src/main/java/com/blog/project/controller/api/UserApiController.java
+++ src/main/java/com/blog/project/controller/api/UserApiController.java
@@ -1,7 +1,10 @@
 package com.blog.project.controller.api;
 
 import com.blog.project.dto.ResponseDto;
+import com.blog.project.model.RoleType;
 import com.blog.project.model.User;
+import com.blog.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserApiController {
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/api/user")
     public ResponseDto<Integer> save(@RequestBody User user) {
         System.out.println("UserApiController: save 호출됨");
-        return new ResponseDto<Integer>(HttpStatus.OK, 1);   // status 값만 보고 정상적으로 수행됐는지를 판단 가능함. 여기서 data는 크게 의미 없음
+        user.setRole(RoleType.USER);
+        userService.회원가입(user);                    // 1이면 성공, -1이면 실패
+
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);   // 자바 오브젝트를 JSON으로 변환해, 리턴(Jackson)
     }
 }

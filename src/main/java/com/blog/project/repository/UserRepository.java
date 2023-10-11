072d9f55d907c481e5b4e70224bec8c61e85d6ee
Sungah <sungah0815@gmail.com>
2023-10-11 14:55:03 +0900

    git commit -m "refactor: 로그인 방식 변경"
    전통적인 로그인 방식에서 스프링 시큐리티 방식으로 변경

--- src/main/java/com/blog/project/repository/UserRepository.java
+++ src/main/java/com/blog/project/repository/UserRepository.java
@@ -9,10 +9,14 @@ import org.springframework.data.jpa.repository.Query;
 // @Repository -> 생략 가능함
 public interface UserRepository extends JpaRepository<User, Integer> {  // JpaRepository가 관리하는 table은 User, 이 table의 primary key는 Integer
 
-    //JPA Naming 전략
-    // SELECT * FROM user WHERE username=? AND password=?;
-    User findByUsernameAndPassword(String username, String password);
+
+}
+
+
+
+//JPA Naming 전략
+// SELECT * FROM user WHERE username=? AND password=?;
+// User findByUsernameAndPassword(String username, String password);
 
 //    @Query(value = "SELECT * FROM user WHERE username=?1 AND password=?2", nativeQuery = true)
 //    User login(String username, String password);
-}

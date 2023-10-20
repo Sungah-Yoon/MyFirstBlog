f6994fef6d255d787bac870a90cb418fd96ecabd
Sungah <sungah0815@gmail.com>
2023-10-20 22:28:15 +0900

    git commit -m "feat: 글 목록 페이징 기능 구현"

--- src/main/java/com/blog/project/controller/BoardController.java
+++ src/main/java/com/blog/project/controller/BoardController.java
@@ -2,6 +2,9 @@ package com.blog.project.controller;
 
 import com.blog.project.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +19,8 @@ public class BoardController {
     // @AuthenticationPrincipal PrincipalDetail principal <- 파라미터로 받아서 사용
 
     @GetMapping({"","/"})
-    public String index(Model model) {
-        model.addAttribute("boards", boardService.글목록());
+    public String index(Model model, @PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        model.addAttribute("boards", boardService.글목록(pageable));
         return "index";
     }
 
--- src/main/java/com/blog/project/service/BoardService.java
+++ src/main/java/com/blog/project/service/BoardService.java
@@ -4,6 +4,8 @@ import com.blog.project.model.Board;
 import com.blog.project.model.User;
 import com.blog.project.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,7 +24,7 @@ public class BoardService {
         boardRepository.save(board);
     }
 
-    public List<Board> 글목록() {
-        return boardRepository.findAll();
+    public Page<Board> 글목록(Pageable pageable) {
+        return boardRepository.findAll(pageable);
     }
 }
